cd3f9c8e1ef42c5fb0598920c95e0c3f73688884
Kawish Gajadien <kawishg@gmail.com>
2021-09-04 20:55:27 +0200

    Delete erroneous sort_five function

--- src/algorithms/sort_five.c
+++ src/algorithms/sort_five.c
@@ -4,25 +4,7 @@
 
 void sort_five(t_stack *stack_a, t_stack *stack_b)
 {
-	int minimum;
-
-    minimum = get_minimum(stack_a);
-	if (is_ordered(stack_a))
-	{
-		rotate_to_top(stack_a, minimum);
-		return;
-	}
-    split_stack_ab(stack_a, stack_b);
-
-    print_stack(stack_a);
-    print_stack(stack_b);
-    printf("is_sorted = %d\n", is_sorted(stack_b));
-
-	sort_three(stack_a, stack_b);
-	while (stack_b->top)
-		push(stack_b, stack_a);
-	if (!is_ordered(stack_a))
-		rotate_to_top(stack_a, minimum);
+    ;
 }
 
 int *create_distance_array(t_stack *stack, int origin)
