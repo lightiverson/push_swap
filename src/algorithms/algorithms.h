a032f9b34e664c21a9f7c7cf2aebbd979ec453e6
Kawish Gajadien <kawishg@gmail.com>
2021-09-04 20:53:07 +0200

    Replace sort_three with new version of sort_three. Works with N is 2 and 2, single stack

--- src/algorithms/algorithms.h
+++ src/algorithms/algorithms.h
@@ -7,7 +7,7 @@
 void bubble_sort(t_stack *stack_a);
 
 // sort_three.c
-void sort_three(t_stack *stack_a, t_stack *stack_b);
+void sort_three(t_stack *stack_a);
 
 // sort_five.c
 void sort_five(t_stack *stack_a, t_stack *stack_b);
