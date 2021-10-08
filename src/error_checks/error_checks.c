38fdfc08b17feb312790ac877d1aeafdb323836c
Kawish Gajadien <kgajadie@f1r2s11.codam.nl>
2021-10-08 17:54:14 +0200

    Norm proof error checks directory

--- src/error_checks/error_checks.c
+++ src/error_checks/error_checks.c
@@ -1,44 +1,44 @@
 #include "../push_swap.h"
 
-void display_err_exit(void)
+void	display_err_exit(void)
 {
-    write(STDERR_FILENO, "Error\n", 6);
-    exit(EXIT_FAILURE);
+	write(STDERR_FILENO, "Error\n", 6);
+	exit(EXIT_FAILURE);
 }
 
-void check_for_int_and_overflow(int argc, char *argv[])
+void	check_for_int_and_overflow(int argc, char *argv[])
 {
-    int i;
-    
-    i = 1;
-    while (i < argc)
-    {
-        mini_strtoi(argv[i]); 
-        i++;
-    }
+	int	i;
+
+	i = 1;
+	while (i < argc)
+	{
+		mini_strtoi(argv[i]);
+		i++;
+	}
 }
 
-void check_for_duplicates(int argc, char *argv[])
+void	check_for_duplicates(int argc, char *argv[])
 {
-    int i;
-    int j;
+	int	i;
+	int	j;
 
-    i = 1;
-    j = 1;
-    while (i < argc)
-    {
-        while (j < argc)
-        {
-            if (i == j)
-            {
-                j++;
-                continue;
-            }
-            if (!strcmp(argv[i], argv[j]))
-                display_err_exit();
-            j++;
-        }
-        j = 1;
-        i++;
-    }
+	i = 1;
+	j = 1;
+	while (i < argc)
+	{
+		while (j < argc)
+		{
+			if (i == j)
+			{
+				j++;
+				continue ;
+			}
+			if (!strcmp(argv[i], argv[j]))
+				display_err_exit();
+			j++;
+		}
+		j = 1;
+		i++;
+	}
 }
--- src/error_checks/error_checks.h
+++ src/error_checks/error_checks.h
@@ -1,8 +1,8 @@
 #ifndef ERROR_CHECKS_H
-#define ERROR_CHECKS_H
+# define ERROR_CHECKS_H
 
-void display_err_exit(void);
-void check_for_int_and_overflow(int argc, char *argv[]);
-void check_for_duplicates(int argc, char *argv[]);
+void	display_err_exit(void);
+void	check_for_int_and_overflow(int argc, char *argv[]);
+void	check_for_duplicates(int argc, char *argv[]);
 
 #endif /* ERROR_CHECKS_H */
