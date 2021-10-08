16a7442fab81cb79b8bfc6623a492ed7fea338fd
Kawish Gajadien <kawishg@outlook.com>
2021-10-08 18:41:12 +0200

    Norm proof setup stacks directory

--- src/setup_stacks/setup_stacks.c
+++ src/setup_stacks/setup_stacks.c
@@ -1,52 +1,52 @@
 #include "../push_swap.h"
 
-void print_stack(const t_stack *stack)
+void	print_stack(const t_stack *stack)
 {
-    int i;
+	int	i;
 
-    i = stack->top;
-    printf("stack->name = %c\n", stack->stack_name);
-    while (i)
-    {
-        printf("[%d] = %d\n", i - 1, stack->p_array[i - 1]);
-        i--;
-    }
-    printf("----\n");
+	i = stack->top;
+	printf("stack->name = %c\n", stack->stack_name);
+	while (i)
+	{
+		printf("[%d] = %d\n", i - 1, stack->p_array[i - 1]);
+		i--;
+	}
+	printf("----\n");
 }
 
-void free_stacks(t_stack *stack_a, t_stack *stack_b)
+void	free_stacks(t_stack *stack_a, t_stack *stack_b)
 {
-    free(stack_a->p_array);
-    free(stack_b->p_array);
+	free(stack_a->p_array);
+	free(stack_b->p_array);
 }
 
-void populate_stack(int argc, char *argv[], t_stack *stack)
+void	populate_stack(int argc, char *argv[], t_stack *stack)
 {
-    int i;
-    int x;
-    int j;
+	int	i;
+	int	x;
+	int	j;
 
-    i = 1;
-    j = stack->len - 1;
-    while (i < argc)
-    {
-        x = mini_strtoi(argv[i]);
-        stack->p_array[j] = x;
-        stack->top++;
-        j--;
-        i++;
-    }
+	i = 1;
+	j = stack->len - 1;
+	while (i < argc)
+	{
+		x = mini_strtoi(argv[i]);
+		stack->p_array[j] = x;
+		stack->top++;
+		j--;
+		i++;
+	}
 }
 
-void initialize_stack(t_stack *stack, int len, char stack_name)
+void	initialize_stack(t_stack *stack, int len, char stack_name)
 {
-    stack->p_array = ft_calloc(len, sizeof(*stack->p_array));
-    if (stack->p_array == NULL)
-    {
-        printf("malloc failed\n");
-        exit(EXIT_FAILURE);
-    }
-    stack->top = 0;
-    stack->len = len;
-    stack->stack_name = stack_name;
+	stack->p_array = ft_calloc(len, sizeof(*stack->p_array));
+	if (stack->p_array == NULL)
+	{
+		printf("malloc failed\n");
+		exit(EXIT_FAILURE);
+	}
+	stack->top = 0;
+	stack->len = len;
+	stack->stack_name = stack_name;
 }
--- src/setup_stacks/setup_stacks.h
+++ src/setup_stacks/setup_stacks.h
@@ -1,9 +1,9 @@
 #ifndef SETUP_STACKS_H
-#define SETUP_STACKS_H
+# define SETUP_STACKS_H
 
-void print_stack(const t_stack *stack);
-void free_stacks(t_stack *stack_a, t_stack *stack_b);
-void populate_stack(int argc, char *argv[], t_stack *stack);
-void initialize_stack(t_stack *stack, int len, char stack_name);
+void	print_stack(const t_stack *stack);
+void	free_stacks(t_stack *stack_a, t_stack *stack_b);
+void	populate_stack(int argc, char *argv[], t_stack *stack);
+void	initialize_stack(t_stack *stack, int len, char stack_name);
 
 #endif /* SETUP_STACKS_H */
