efa8c7177acda3b2baff30c2e2fadfb3e2233d83
Kawish Gajadien <kgajadie@f1r2s11.codam.nl>
2021-10-08 17:51:39 +0200

    Norm proof algo directory

--- src/algorithms/sort_three.c
+++ src/algorithms/sort_three.c
@@ -1,21 +1,21 @@
 #include "../push_swap.h"
 
-void sort_three(t_stack *stack_a)
+void	sort_three(t_stack *stack_a)
 {
-	int minimum;
-	int index_minimum;
+	int	minimum;
+	int	index_minimum;
 
-    minimum = get_minimum(stack_a);
-    index_minimum = get_index(stack_a, minimum);
+	minimum = get_minimum(stack_a);
+	index_minimum = get_index(stack_a, minimum);
 	if (is_sorted(stack_a))
-		return;
+		return ;
 	if (is_ordered(stack_a))
 	{
 		if (index_minimum == 1)
 			rotate_a_or_b(stack_a);
 		else if (index_minimum == 0)
 			reverse_rotate_a_or_b(stack_a);
-		return;
+		return ;
 	}
 	if (index_minimum == 2)
 		reverse_rotate_a_or_b(stack_a);
--- src/algorithms/utilities_algorithms.c
+++ src/algorithms/utilities_algorithms.c
@@ -1,38 +1,38 @@
 #include "../push_swap.h"
 
-int rotate_to_top(t_stack *stack, int element)
+int	rotate_to_top(t_stack *stack, int element)
 {
-    int number_of_rotations;
-    int index_element;
-    int middle_of_stack;
+	int	number_of_rotations;
+	int	index_element;
+	int	middle_of_stack;
 
-    number_of_rotations = 0;
-    index_element = get_index(stack, element);
-    middle_of_stack = (stack->top - 1) / 2;
-    while (get_index(stack, element) != stack->top - 1)
-    {
-        if (index_element < middle_of_stack)
-            reverse_rotate_a_or_b(stack);
-        else
-            rotate_a_or_b(stack);
-        number_of_rotations++;
-    }
-    return (number_of_rotations);
+	number_of_rotations = 0;
+	index_element = get_index(stack, element);
+	middle_of_stack = (stack->top - 1) / 2;
+	while (get_index(stack, element) != stack->top - 1)
+	{
+		if (index_element < middle_of_stack)
+			reverse_rotate_a_or_b(stack);
+		else
+			rotate_a_or_b(stack);
+		number_of_rotations++;
+	}
+	return (number_of_rotations);
 }
 
-void split_stack_ab(t_stack *stack_a, t_stack *stack_b)
+void	split_stack_ab(t_stack *stack_a, t_stack *stack_b)
 {
-    int i;
-    int maximum;
-    int elements_to_push;
+	int	i;
+	int	maximum;
+	int	elements_to_push;
 
-    i = 0;
-    elements_to_push = (stack_a->top - 1) / 2;
-    while (i < elements_to_push)
-    {
-        maximum = get_maximum(stack_a);
-        rotate_to_top(stack_a, maximum);
-        push(stack_a, stack_b);
-        i++;
-    }
+	i = 0;
+	elements_to_push = (stack_a->top - 1) / 2;
+	while (i < elements_to_push)
+	{
+		maximum = get_maximum(stack_a);
+		rotate_to_top(stack_a, maximum);
+		push(stack_a, stack_b);
+		i++;
+	}
 }
