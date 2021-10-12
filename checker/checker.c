bb50a981b5ebbce63081212bfe521c1e08a03e84
Kawish Gajadien <kawishg@outlook.com>
2021-10-12 12:38:54 +0200

    Norm proof checker.* files

--- checker/checker.c
+++ checker/checker.c
@@ -1,54 +1,50 @@
 #include "./checker.h"
 
-void execute_operations(t_stack *stack_a, t_stack *stack_b, char *operation)
+void	execute_operations(t_stack *stack_a, t_stack *stack_b, char *operation)
 {
-    if (!strcmp(operation, "sa"))
-        swap_a_or_b(stack_a);
-    else if (!strcmp(operation, "sb"))
-        swap_a_or_b(stack_b);
-    else if (!strcmp(operation, "ss"))
-        swap_ab(stack_a, stack_b);
-    else if (!strcmp(operation, "pa"))
-        push(stack_a, stack_b);
-    else if (!strcmp(operation, "pb"))
-        push(stack_b, stack_a);
-    else if (!strcmp(operation, "ra"))
-        rotate_a_or_b(stack_a);
-    else if (!strcmp(operation, "rb"))
-        rotate_a_or_b(stack_b);
-    else if(!strcmp(operation, "rr"))
-        rotate_ab(stack_a, stack_b);
-    else if (!strcmp(operation, "rra"))
-        reverse_rotate_a_or_b(stack_a);
-    else
-        display_err_exit();
+	if (!strcmp(operation, "sa"))
+		swap_a_or_b(stack_a);
+	else if (!strcmp(operation, "sb"))
+		swap_a_or_b(stack_b);
+	else if (!strcmp(operation, "ss"))
+		swap_ab(stack_a, stack_b);
+	else if (!strcmp(operation, "pa"))
+		push(stack_a, stack_b);
+	else if (!strcmp(operation, "pb"))
+		push(stack_b, stack_a);
+	else if (!strcmp(operation, "ra"))
+		rotate_a_or_b(stack_a);
+	else if (!strcmp(operation, "rb"))
+		rotate_a_or_b(stack_b);
+	else if (!strcmp(operation, "rr"))
+		rotate_ab(stack_a, stack_b);
+	else if (!strcmp(operation, "rra"))
+		reverse_rotate_a_or_b(stack_a);
+	else
+		display_err_exit();
 }
 
-int main(int argc, char *argv[])
+int	main(int argc, char *argv[])
 {
-    t_stack stacks[2];
-    char **line;
+	t_stack	stacks[2];
+	char	**line;
 
-    if (argc <= 1)
-        exit(EXIT_FAILURE);
-    check_for_int_and_overflow(argc, argv);
-    check_for_duplicates(argc, argv);
-    initialize_stack(&(stacks[A]), argc - 1, 'a');
-    populate_stack(argc, argv, &(stacks[A]));
-    initialize_stack(&(stacks[B]), argc - 1, 'b');
-
-    print_stack(&(stacks[A]));
-    print_stack(&(stacks[B]));
-
-    while (get_next_line(0, line))
-        execute_operations(&(stacks[A]), &(stacks[B]), *line);
-
-    print_stack(&(stacks[A]));
-    print_stack(&(stacks[B]));
-
-    if (is_sorted(&(stacks[A])) && stacks[B].top == 0)
-        write(1, "OK\n", 3);
-    else
-        write(1, "KO\n", 3);
-    return (0);
+	if (argc <= 1)
+		exit(EXIT_FAILURE);
+	check_for_int_and_overflow(argc, argv);
+	check_for_duplicates(argc, argv);
+	initialize_stack(&(stacks[A]), argc - 1, 'a');
+	populate_stack(argc, argv, &(stacks[A]));
+	initialize_stack(&(stacks[B]), argc - 1, 'b');
+	print_stack(&(stacks[A]));
+	print_stack(&(stacks[B]));
+	while (get_next_line(0, line))
+		execute_operations(&(stacks[A]), &(stacks[B]), *line);
+	print_stack(&(stacks[A]));
+	print_stack(&(stacks[B]));
+	if (is_sorted(&(stacks[A])) && stacks[B].top == 0)
+		write(1, "OK\n", 3);
+	else
+		write(1, "KO\n", 3);
+	return (0);
 }
--- checker/checker.h
+++ checker/checker.h
@@ -1,10 +1,10 @@
 #ifndef CHECKER_H
-#define CHECKER_H
+# define CHECKER_H
 
 // Check welke libraries mogen!!!
 # include <string.h>
 
-#include "../src/push_swap.h"
-#include "./get_next_line/get_next_line.h"
+# include "../src/push_swap.h"
+# include "./get_next_line/get_next_line.h"
 
 #endif /* CHECKER_H */
