ac52566091b093a0ff0b23ee739752b36879c2d9
Kawish Gajadien <kawishg@outlook.com>
2021-10-12 10:22:33 +0200

    Norm proof mini_strtoi.c

--- src/mini_strtoi.c
+++ src/mini_strtoi.c
@@ -1,44 +1,46 @@
 #include "push_swap.h"
 
-static void handle_negative(int *p_number, int *p_digit)
+static void	handle_negative(int *p_number, int *p_digit)
 {
-    if ((*p_number > INT_MAX / 10) || ((*p_number == INT_MAX / 10) && (*p_digit > INT_MAX % 10)))
-        display_err_exit();
-    *p_number = (*p_number * 10) + *p_digit;
+	if ((*p_number > INT_MAX / 10) || ((*p_number == INT_MAX / 10)
+			&& (*p_digit > INT_MAX % 10)))
+		display_err_exit();
+	*p_number = (*p_number * 10) + *p_digit;
 }
 
-static void handle_positive(int *p_number, int *p_digit)
+static void	handle_positive(int *p_number, int *p_digit)
 {
-    if ((*p_number < INT_MIN / 10) || ((*p_number == INT_MIN / 10) && (*p_digit > (INT_MIN % 10) * -1)))
-        display_err_exit();
-    *p_number = (*p_number * 10) - *p_digit;
+	if ((*p_number < INT_MIN / 10) || ((*p_number == INT_MIN / 10)
+			&& (*p_digit > (INT_MIN % 10) * -1)))
+		display_err_exit();
+	*p_number = (*p_number * 10) - *p_digit;
 }
 
-int mini_strtoi(const char *str)
+int	mini_strtoi(const char *str)
 {
-    bool is_negative;
-    int number;
-    int digit;
+	bool	is_negative;
+	int		number;
+	int		digit;
 
-    if (*str == '-')
-    {
-        is_negative = true;
-        str++;
-    }
-    number = 0;
-    digit = 0;
-    if (*str == '\0')
-        display_err_exit();
-    while (*str)
-    {
-        if (!ft_isdigit(*str))
-            display_err_exit();
-        digit = *str - '0';
-        if (!is_negative)
-            handle_negative(&number, &digit);
-        else
-            handle_positive(&number, &digit);
-        str++;
-    }
-    return (number);
+	if (*str == '-')
+	{
+		is_negative = true;
+		str++;
+	}
+	number = 0;
+	digit = 0;
+	if (*str == '\0')
+		display_err_exit();
+	while (*str)
+	{
+		if (!ft_isdigit(*str))
+			display_err_exit();
+		digit = *str - '0';
+		if (!is_negative)
+			handle_negative(&number, &digit);
+		else
+			handle_positive(&number, &digit);
+		str++;
+	}
+	return (number);
 }
