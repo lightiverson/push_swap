b082a940b926e219bf35e46e7a678a08b86724bd
Kawish Gajadien <kawishg@outlook.com>
2021-10-27 15:18:56 +0200

    NA

--- checker/checker.c
+++ checker/checker.c
@@ -34,24 +34,42 @@ void	execute_operations(t_stack *stack_a, t_stack *stack_b, char *operation)
 int	main(int argc, char *argv[])
 {
 	t_stack	stacks[2];
+	int		is_malloc_failed;
 	char	**line;
+
+	is_malloc_failed = 0;
 
 	if (argc <= 1)
 		exit(EXIT_FAILURE);
+
+// ##############
+// Checks & setup - kan in 1 functie
+// ##############
 	check_for_int_and_overflow(argc, argv);
-	initialize_stack(&(stacks[A]), argc - 1, 'a');
+	
+	is_malloc_failed = initialize_stack(&(stacks[A]), argc - 1, 'a');
+	if (is_malloc_failed)
+		exit(EXIT_FAILURE);
+
 	populate_stack(argc, argv, &(stacks[A]));
-	initialize_stack(&(stacks[B]), argc - 1, 'b');
+
+	is_malloc_failed = initialize_stack(&(stacks[B]), argc - 1, 'b');
+	if (is_malloc_failed)
+	{
+		free(&(stacks[A]).p_array);
+		exit(EXIT_FAILURE);
+	}
+
 	exit_if_duplicate(&(stacks[A]), &(stacks[B]));
-	print_stack(&(stacks[A]));
-	print_stack(&(stacks[B]));
+
 	while (get_next_line(0, line))
 		execute_operations(&(stacks[A]), &(stacks[B]), *line);
-	print_stack(&(stacks[A]));
-	print_stack(&(stacks[B]));
+
 	if (is_sorted(&(stacks[A])) && stacks[B].top == 0)
 		write(1, "OK\n", 3);
 	else
 		write(1, "KO\n", 3);
+
+	free_stacks(&(stacks[A]), &(stacks[B]));
 	return (0);
 }
