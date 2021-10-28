059cb66b7ff481126bba75fc8849594ac3a435cb
Kawish Gajadien <kawishg@outlook.com>
2021-10-28 12:53:57 +0200

    Replace printf with multiple writes in push operation

--- src/operations/push.c
+++ src/operations/push.c
@@ -4,7 +4,10 @@ int	push(t_stack *origin_stack, t_stack *destination_stack)
 {
 	int	first_element;
 
-	printf("p%c\n", destination_stack->stack_name);
+	// printf("p%c\n", destination_stack->stack_name);
+	write(1, "p", 1);
+	write(1, &(destination_stack->stack_name), 1);
+	write(1, "\n", 1);
 	if (origin_stack->top == 0)
 		return (1);
 	first_element = origin_stack->p_array[origin_stack->top - 1];
