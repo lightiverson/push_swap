71c1b6a51adf2821ed91f9077a976f8a9eeb5883
Kawish Gajadien <kawishg@outlook.com>
2021-10-28 17:05:14 +0200

    Clean checker.h

--- checker/checker.h
+++ checker/checker.h
@@ -1,10 +1,13 @@
 #ifndef CHECKER_H
 # define CHECKER_H
 
-// Check welke libraries mogen!!!
-# include <string.h>
-
-# include "../src/push_swap.h"
 # include "./get_next_line/get_next_line.h"
+# include "../src/struct.h"
+# include "../src/setup_stacks/setup_stacks.h"
+# include "./operations/operations.h"
+# include "../src/is/is.h"
+
+# define A   0
+# define B   1
 
 #endif /* CHECKER_H */
